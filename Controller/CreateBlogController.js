3f0ea4d4997e335191bb89f8c715577d85253516
shubham pandey <piscShubhamPandey@outlook.com>
2024-03-21 07:06:17 +0530

    loginapi

--- Controller/CreateBlogController.js
+++ Controller/CreateBlogController.js
@@ -5,16 +5,10 @@ const con=require('../Database/config')
 const createBlog={
     Blog: async (req,res)=>{
         const data=req.body;
-
         const query='insert into blogdetails set ?';
-
         const result= await execute(query,data);
-
-        res.send(result);
-        console.log(result)
     },
     getBlog:async(req,res)=>{
-       
         const page = req.query.page || 1;
         const perPage = parseInt(req.query.perPage) || 10; // Ensure perPage is parsed as an integer
         const offset = (page - 1) * perPage;
@@ -25,13 +19,7 @@ const createBlog={
             }
          
             else{
-                let totalitem=blogitem[0].totalitem
-            
-            console.log(totalitem)
-        
-
-          
-            
+                let totalitem=blogitem[0].totalitem     
             con.query('SELECT * FROM blogdetails LIMIT ? OFFSET ?', [perPage, offset], (err, result) => {
                 if (err) {
                     throw err;
